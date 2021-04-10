726f113cfc485f971cfe039a9123dfb00e2f0bdb
Kouhei Suzuki <kouhei.szk@gmail.com>
2021-04-10 23:55:44 +0900

    Initial Commit

---
+++ StreamAudioPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StreamAudioPlayer
+//
+//  Created by Kouhei Suzuki on 2021/04/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
